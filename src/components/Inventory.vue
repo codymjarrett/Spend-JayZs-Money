61dd6f45f314b6d75f0e2fd360bc681c70b6823b
Hero's Dad <cody.monta@gmail.com>
2019-11-17 22:53:07 -0600

    preparing to add vuex

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -2,16 +2,32 @@
   <div class="container">
     <ul class="flex flex-wrap mb-4 -mx-2">
       <li class="w-1/3 px-2 mb-4" v-for="item in inventory" :key="item.id" :data-id="item.id">
-        <div class="rounded overflow-hidden shadow-lg">
+        <div class="rounded overflow-hidden shadow-lg min-h-full">
           <div
-            class="bg-contain bg-white bg-no-repeat bg-center h-40"
+            class="bg-white bg-no-repeat bg-center h-40"
+            :class="item.fullSize ? 'bg-cover' : 'bg-contain'"
             :style="{backgroundImage: `url(${item.image})` }"
           ></div>
-          <div class="p-4 bg-primary-light text-center text-2xl">{{item.name}}</div>
-          <div class="text-center p-4 text-2xl">${{item.price}}</div>
-          <div class="flex justify-around">
-            <button class="bg-secondary hover:bg-primary p-4 text-white rounded">Sell</button>
-            <input class="text-center" type="number" min="0" v-model="item.quantity" />
+          <div class="p-4 bg-primary-light text-center text-2xl max-h-full">{{item.name}}</div>
+          <div class="text-center p-4 text-2xl">${{formatNumber(item.price)}}</div>
+          <div class="flex flex-wrap justify-around pb-4">
+            <!-- find out how to break this out into a computed function -->
+            <button v-on:click="alert('hello')" class="bg-secondary p-4 text-white rounded" :class="renderDisabledButton(item)">Sell</button>
+            <div class="relative">
+              <button
+                class="decrement bg-tetriary text-white p-4 absolute top-0 left-0"
+                :class="item.quantity == 0 &&'opacity-50 cursor-not-allowed pointer-events-none'"
+                v-on:click="decrement(item)"
+                tabindex="0"
+              >&#45;</button>
+              <input class="text-center p-4" type="number" min="0" v-model="item.quantity" />
+              <button
+                class="increment bg-tetriary text-white p-4 absolute top-0 right-0"
+                v-on:click="increment(item)"
+                v-on:keyup.enter="item.quantity++"
+                tabindex="0"
+              >&#43;</button>
+            </div>
             <button
               class="bg-secondary hover:bg-primary p-4 text-white rounded"
               v-on:click="sendPurchase(item.id)"
@@ -35,7 +51,7 @@ export default {
   data: () => {
     return {
       inventory: inventory,
-      quantity: 0
+      purchaseMade: false
     };
   },
   methods: {
@@ -53,24 +69,55 @@ export default {
       return item[0];
     },
     clearQuantity: function() {
-        this.inventory.map(i => {
-            i.quantity = 0
-        })
+      this.inventory.map(i => {
+        i.quantity = 0;
+      });
     },
     sendPurchase: function(id) {
       const price = this.getItemAmount(id);
       const quantity = this.getItemQuantity(id);
       const total = price * quantity;
       eventBus.$emit("total-purchase", total);
-      this.clearQuantity()
+      this.clearQuantity();
+    },
+    // sellPurchase: function(id) {
+    //   if()
+    // }
+    formatNumber: function(price) {
+      return price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    },
+    increment: function(arg) {
+      arg.quantity++;
+      this.purchaseMade = true;
+    },
+    decrement: function(arg) {
+      return arg.quantity--;
+    },
+
+    renderDisabledButton: function(arg) {
+      if (arg.quantity > 0 && this.purchaseMade) {
+        return "cursor-pointer opacity-100";
+      } else if (arg.quantity == 0) {
+        return "opacity-50 cursor-not-allowed";
+      }
     }
   },
+  computed: {},
   watch: {
-    getItemQuantity: function() {}
+    // getItemQuantity: function() {}
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input[type="number"] {
+  -webkit-appearance: textfield;
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
 </style>
--- src/inventory/inventory.js
+++ src/inventory/inventory.js
@@ -1,11 +1,12 @@
 export const inventory = [
 	{
 		id: 1,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'A Bar of gold',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://360view.hum3d.com/zoom/Tools/Gold_Bar_1000_0001.jpg',
+		price: 50000,
 		quantity: 0,
+		fullSize: true,
 	},
 	{
 		id: 2,
@@ -18,27 +19,29 @@ export const inventory = [
 	},
 	{
 		id: 3,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'Private Jet',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://beam.land/static/images/p/large_1407_90108.png',
+		fullSize: true,
+		price: 40000000,
 		quantity: 0,
 	},
 	{
 		id: 4,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'iPhone 11 Pro Max',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-max-midnight-green-select-2019?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1566953859350',
+		price: 1449,
 		quantity: 0,
 	},
 	{
 		id: 5,
-		name: 'Cosmograph Daytona Rolex',
+		name: "Popeye's Chicken Sandwich",
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/wp-cms/uploads/2019/10/p-1-popeyes-chicken-sandwich-breakdown.jpg',
+		price: 3.99,
 		quantity: 0,
+		fullSize: true,
 	},
 	{
 		id: 6,
@@ -50,50 +53,29 @@ export const inventory = [
 	},
 	{
 		id: 7,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'Pumpkin Spice Latte',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://image.businessinsider.com/5d5177f1cd97844f96466824?width=1100&format=jpeg&auto=webp',
+		price: 5.25,
 		quantity: 0,
+		fullSize: true,
 	},
 	{
 		id: 8,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'NFL Superbowl Ad',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://static.nfl.com/static/content/public/static/img/share/shield.jpg',
+		price: 5000000,
 		quantity: 0,
+		fullSize: true,
 	},
 	{
 		id: 9,
-		name: 'Cosmograph Daytona Rolex',
+		name: 'Netflix Subscription',
 		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
+			'https://images-na.ssl-images-amazon.com/images/I/616QXs8yg0L.png',
+		price: 12.99,
 		quantity: 0,
-	},
-	{
-		id: 10,
-		name: 'Cosmograph Daytona Rolex',
-		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
-		quantity: 0,
-	},
-	{
-		id: 11,
-		name: 'Cosmograph Daytona Rolex',
-		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
-		quantity: 0,
-	},
-	{
-		id: 12,
-		name: 'Cosmograph Daytona Rolex',
-		image:
-			'https://images.rolex.com/2019/catalogue/images/upright-bba-with-shadow/m116508-0008.png?impolicy=upright-majesty',
-		price: 36600,
-		quantity: 0,
-	},
+		fullSize: true,
+	}
 ]
